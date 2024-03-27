8121640c7a562649e96bec9c3b06f4c9c791e7dc
rajkanya-chaudhary <dc5068786@gmail.com>
2024-03-27 22:26:36 +0530

    this is a react projrct

---
+++ src/FunctionComponats/InputExample.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+export default function InputExample() {
+    var [data,setData] = useState({
+          name : " "
+
+
+
+    })
+  return (
+    <>
+      <div className="main">
+        <div className="center">
+            <h3>Function Componants Input Example </h3>
+        </div>
+      </div>
+    </>
+  )
+}
